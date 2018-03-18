eeaa1b844cfe1b7f7fcaf0090d610bec439dc9e1
vneogi199 <vneogi199@gmail.com>
2018-03-18 17:42:37 +0000

    Algorithmia website commit

--- src/ScikitRandomForest.py
+++ src/ScikitRandomForest.py
@@ -1,7 +1,26 @@
 import Algorithmia
 import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+np.random.seed(0)
+
+
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
-    print(pd)
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df.head()
+    df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
+    df.head()
+    df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
+    df.head()
+    train, test = df[df['is_train']==True], df[df['is_train']==False]
+    features = df.columns[:4]
+    features
+    
+    
+    
+    
     return "hello {}".format(input)
