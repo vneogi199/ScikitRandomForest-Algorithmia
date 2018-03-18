e99c025b942538fb6a0c36d6d53207a8472f2192
vneogi199 <vneogi199@gmail.com>
2018-03-18 17:37:44 +0000

    Algorithmia website commit

--- src/ScikitRandomForest.py
+++ src/ScikitRandomForest.py
@@ -1,6 +1,7 @@
 import Algorithmia
-
+import pandas as pd
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
+    print(pd)
     return "hello {}".format(input)
