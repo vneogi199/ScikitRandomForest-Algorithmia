5d995ef0d176ee8f1c025f1641f4852e68fffaf4
vneogi199 <vneogi199@gmail.com>
2018-03-18 17:44:52 +0000

    Algorithmia website commit

--- src/ScikitRandomForest.py
+++ src/ScikitRandomForest.py
@@ -11,14 +11,19 @@ np.random.seed(0)
 def apply(input):
     iris = load_iris()
     df = pd.DataFrame(iris.data, columns=iris.feature_names)
-    df.head()
     df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
-    df.head()
     df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
-    df.head()
     train, test = df[df['is_train']==True], df[df['is_train']==False]
     features = df.columns[:4]
-    features
+    y = pd.factorize(train['species'])[0]
+    clf = RandomForestClassifier(n_jobs=2, random_state=0)
+    clf.fit(train[features], y)
+    clf.predict(test[features])
+    clf.predict_proba(test[features])[0:10]
+    preds = iris.target_names[clf.predict(test[features])]
+    print(preds[0:5])
+    pd.crosstab(test['species'], preds, rownames=['Actual Species'], colnames=['Predicted Species'])
+    print(list(zip(train[features], clf.feature_importances_)))
     
     
     
