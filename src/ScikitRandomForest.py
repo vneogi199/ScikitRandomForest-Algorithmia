0100e6366ea4f01950fbc54181da607877fddb58
vneogi199 <vneogi199@gmail.com>
2018-03-19 13:21:18 +0000

    Algorithmia website commit

--- src/ScikitRandomForest.py
+++ src/ScikitRandomForest.py
@@ -1,31 +1,23 @@
 import Algorithmia
 import pandas as pd
-from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
-import numpy as np
-np.random.seed(0)
-
-
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
+def random_forest_classifier(features, target):
+    """
+    To train the random forest classifier with features and target data
+    :param features:
+    :param target:
+    :return: trained random forest classifier
+    """
+    clf = RandomForestClassifier()
+    clf.fit(features, target)
+    return clf
+
 def apply(input):
-    iris = load_iris()
-    df = pd.DataFrame(iris.data, columns=iris.feature_names)
-    df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
-    df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
-    train, test = df[df['is_train']==True], df[df['is_train']==False]
-    features = df.columns[:4]
-    y = pd.factorize(train['species'])[0]
-    clf = RandomForestClassifier(n_jobs=2, random_state=0)
-    clf.fit(train[features], y)
-    clf.predict(test[features])
-    clf.predict_proba(test[features])[0:10]
-    preds = iris.target_names[clf.predict(test[features])]
-    print(preds[0:5])
-    pd.crosstab(test['species'], preds, rownames=['Actual Species'], colnames=['Predicted Species'])
-    print(list(zip(train[features], clf.feature_importances_)))
-    
-    
-    
-    
+    dataset = pd.read_csv('data://vneogi199/training/Algorithmia-phishing1.arff')
+    trained_model = random_forest_classifier(dataset[:,:29], dataset[:,29])
+    print("Trained model :: "+ trained_model)
     return "hello {}".format(list(zip(train[features], clf.feature_importances_)))
