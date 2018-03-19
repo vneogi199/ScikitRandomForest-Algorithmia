bf0a32904f62b3f759e4a0515b2a2c26c9a9f977
vneogi199 <vneogi199@gmail.com>
2018-03-19 17:03:32 +0000

    Algorithmia website commit

--- src/ScikitRandomForest.py
+++ src/ScikitRandomForest.py
@@ -16,9 +16,21 @@ def random_forest_classifier(features, target):
     clf.fit(features, target)
     return clf
 
+def process_input(input):
+    # Create numpy array from csv file passed as input in apply()
+    if input.startswith('data:'):
+        file_url = client.file(input).getFile().name
+        try:
+            np_array = np.genfromtxt(file_url, delimiter=',')
+            print(np_array)
+            return np_array
+        except Exception as e:
+            print("Could not create numpy array from data", e)
+            sys.exit(0)
+
+
 def apply(input):
-    # dataset = pd.read_csv('data://vneogi199/training/Algorithmia-phishing.csv')
-    dataset = pd.read_csv('data://.my/training/Algorithmia-phishing.csv')
+    dataset = process_input(data://vneogi199/training/Algorithmia-phishing.csv)
     trained_model = random_forest_classifier(dataset[:,:29], dataset[:,29])
     print("Trained model :: "+ trained_model)
     return "hello {}".format(list(zip(train[features], clf.feature_importances_)))
